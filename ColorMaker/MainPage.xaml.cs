5d0d50544b15bc1271be24adcc057771744570e7
zirocool2020 <zirocool2003@hotmail.com>
2022-05-26 22:01:09 -0300

    26-05-22
    proyecto final

---
+++ ColorMaker/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace ColorMaker;
+
+public partial class MainPage : ContentPage
+{
+    bool esAleatorio;
+    string ValorHex;
+
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+    private void SliderValorCambiado(object sender, ValueChangedEventArgs e)
+    {
+        if (!esAleatorio)
+        {
+            var rojo = SldRojo.Value;
+            var verde = SldVerde.Value;
+            var azul = SldAzul.Value;
+
+            Color color = Color.FromRgb(rojo, verde, azul);
+
+            SetearColor(color);
+        }
+		
+
+    }
+
+    private void SetearColor(Color color)
+    {
+        btnAleatorio.BackgroundColor = color;
+        Contenedor.BackgroundColor = color;
+        ValorHex= color.ToHex();
+        lblHex.Text = ValorHex;
+    }
+
+    private void btnAleatorio_presionado(object sender, EventArgs e)
+    {
+        esAleatorio = true;
+        var random = new Random();
+
+        var color = Color.FromRgb
+            (
+            random.Next(0, 256),
+            random.Next(0, 256),
+            random.Next(0, 256)
+            );
+        SetearColor(color);
+
+        SldRojo.Value = color.Red;
+        SldVerde.Value = color.Green;
+        SldAzul.Value = color.Blue;
+        esAleatorio = false;
+
+    }
+
+    private async void imgBtn_presionado(object sender, EventArgs e)
+    {
+        await Clipboard.SetTextAsync(ValorHex);
+        var toast = Toast.Make("Color Copiado",
+            CommunityToolkit.Maui.Core.ToastDuration.Short,
+            12);
+        await toast.Show();
+    }
+}
+
